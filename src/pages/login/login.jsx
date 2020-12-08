a0f8839bdb99e8a3088c324aab023ebabf5bdf4d
churry <bc981119@163.com>
2020-12-08 21:08:08 +0800

    用户输入框样式更新

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './login.less';
 import logo from './images/logo.png';
-import { Form, Icon, Input, Button } from 'antd';
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 /**
  * 登陆的路由组件
  */
 export default class Login extends Component {
+
+    handleSubmit = () => {
+        return;
+    };
+
     render() {
         return (
             <div className='login'>
@@ -20,18 +26,22 @@ export default class Login extends Component {
                         name="normal_login"
                         className="login-form"
                         initialValues={{ remember: true }}
+                        onFinish={this.handleSubmit}
                     >
                         <Form.Item
+                            prefix={<LockOutlined className="site-form-item-icon" />}
                             name="username"
-                            rules={[{ required: true, message: 'Please input your Username!' }]}
+                            rules={[{ required: true, message: '请输入用户名!' }]}
                         >
-                            <Input placeholder="Username" />
+                            <Input placeholder="Username" prefix={<UserOutlined className="site-form-item-icon" />} />
                         </Form.Item>
                         <Form.Item
+
                             name="password"
-                            rules={[{ required: true, message: 'Please input your Password!' }]}
+                            rules={[{ required: true, message: '请输入密码!' }]}
                         >
                             <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
                                 type="password"
                                 placeholder="Password"
                             />
